c0b7804465ddebf1e97c92cb33b9d5e27365ddd7
AMD <michojekunle1@gmail.com>
2023-03-14 09:10:14 +0100

    made changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,18 @@
+const chatForm = document.getElementById('chat-form');
+
 const socket = io();
+
+socket.on('message', message => {
+    console.log(message);
+})
+
+// Message Submit
+chatForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    //Get message Text
+    const msg = e.target.elements.msg.value;
+
+    //Emitin gthe message to the server
+    socket.emit('chatMessage', msg);
+})
--- server.js
+++ server.js
@@ -13,8 +13,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Run when client connects
 io.on('connection', socket => {
     console.log('New WS Connection...');
+    
+    socket.emit('message', 'Welcome to ChatCord');
+
+    // Broadcast when a user connects
+    socket.broadcast.emit('message', 'A user has joined the chat')
+
+    // Broadcast when a user disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat')
+    })
+
+    //listen for chatMessage
+    socket.on('chatMessage', msg => {
+        io.emit('message', msg);
+    })
 })
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {console.log(`Server Running on port ${PORT}`)});
